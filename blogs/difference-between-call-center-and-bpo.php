c74262782f3b4a4445669e232aea6195d48b7e74
shwetasingh2512 <shwetasingh230997@gmail.com>
2025-07-14 18:23:34 +0530

    blog-page

--- blogs/difference-between-call-center-and-bpo.php
+++ blogs/difference-between-call-center-and-bpo.php
@@ -372,7 +372,7 @@
             <div class="row" data-sticky_parent>
                 <div class="col-xl-8" data-sticky_column>
                     <div class="media mb-40 mb-md-35 mb-sm-30 mb-xs-25">
-                        <img src="../assets/img/blog/blog-1.webp" alt="project details">
+                        <img src="../assets/img/blog/blog-14.webp" alt="project details">
                     </div>
 
                     <div class="services-details__content">
