3cafe1f37f3fea9083cffeb8c71a8daf27b29a38
shwetasingh2512 <shwetasingh230997@gmail.com>
2025-07-18 18:37:59 +0530

    blogs

--- blog.php
+++ blog.php
@@ -943,7 +943,7 @@
                     <div class="blog-item mb-30">
                         <div class="blog-featured-thumb mb-xs-30 mb-sm-30 mb-md-35 mb-lg-40 mb-50">
                             <div class="media overflow-hidden">
-                                <a href="./blogs/why-is-marketing-and-branding-important-for-business">
+                                <a href="./blogs/how-to-purchase-omnichannel-customer-support-services">
                                     <img src="./assets/img/blog/blog-11.webp" class="img-fluid" alt="office time" width="434" height="300" loading="lazy">
                                 </a>
                             </div>
@@ -956,11 +956,11 @@
 
                         <div class="content pr-sm-25 pr-xs-15 pl-xs-15 pl-sm-25 pr-xs-15 pr-30 pb-30 pl-30">
                             <div class="post-author mb-2">
-                                <a href="./blogs/why-is-marketing-and-branding-important-for-business">Omnichannel Customer Services </a>
+                                <a href="./blogs/how-to-purchase-omnichannel-customer-support-services">Omnichannel Customer Services </a>
                             </div>
-                            <h4><a href="./blogs/why-is-marketing-and-branding-important-for-business">How to Purchase Omnichannel Customer Support Services</a></h4>
+                            <h4><a href="./blogs/how-to-purchase-omnichannel-customer-support-services">How to Purchase Omnichannel Customer Support Services</a></h4>
                             <div class="btn-link-share mt-xs-10 mt-sm-10 mt-15">
-                                <a href="./blogs/why-is-marketing-and-branding-important-for-business" class="theme-btn btn-border">Read More</a>
+                                <a href="./blogs/how-to-purchase-omnichannel-customer-support-services" class="theme-btn btn-border">Read More</a>
                             </div>
                         </div>
                     </div>
