7989c85aad0131202986aa8d5635744b7d2fde56
shwetasingh2512 <shwetasingh230997@gmail.com>
2025-09-01 11:52:36 +0530

    meta tags update

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ $services = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="msvalidate.01" content="049B8650933642FA0121BEA43665B8D4" />
     <!-- ------------------------- meta tags ----------------------------- -->
     
     <title> India's Best BPO Service Provider Company | Razor Infotech</title>
@@ -61,6 +62,8 @@ $services = [
     <link rel="canonical" href="https://razorinfotech.com/">
     <!-- ===========  All Stylesheet ================= -->
     <?php include 'include/head-style.php' ?>
+
+    
 
     <script type="application/ld+json">
 <?php
